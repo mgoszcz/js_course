e315f1d6835864bd539113988244905400abc682
mgoszczynski <marcin.goszczynski88@gmail.com>
2022-09-29 22:54:46 +0200

    Data structures: Maps

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -405,7 +405,7 @@ console.log(entries);
 for (const [key, {open, close}] of entries) {
   console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
-*/
+
 
 // ############################ Sets
 // collection of unique values
@@ -443,3 +443,79 @@ console.log(new Set(staff).size); // just get count of unique positions
 
 //counting letters
 console.log(new Set('MarcinGoszczynski').size);
+
+*/
+
+// ############################ Maps
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// set returns a map so can be chained
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 10;
+console.log(rest.get(time >= rest.get('open') && time <= rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+rest.set([1, 2], 'Test');
+rest.set(document.querySelector('h1'), 'heading');
+console.log(rest);
+console.log(rest.size);
+// rest.clear()
+
+console.log(rest.get([1, 2])); //returns undefined as that arrays are two different objects
+
+const question = new Map([
+  ['question', 'What is the best programming language?'],
+  [1, 'C'],
+  [2, 'JAva'],
+  [3, 'JAvaScript'],
+  ['correct', 3],
+  [true, 'Correct answer'],
+  [false, 'Wrong answer'],
+]);
+console.log(question);
+
+//convert object to map
+const openingHours = restaurant.openingHours;
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// print keys
+for (const key of question.keys()) {
+  console.log(key);
+}
+
+// print values
+for (const value of question.values()) {
+  console.log(value);
+}
+
+// print answers only
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+//quiz game
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+// const answer = Number(prompt('Answer: '));
+// console.log(question.get(answer === question.get('correct')));
+
+// Convert map to array
+console.log([...question]);
+console.log([...question.entries()]);
