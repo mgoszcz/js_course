9d8a6606f75a8788a79df72701b7cda911227962
mgoszczynski <marcin.goszczynski88@gmail.com>
2022-11-15 19:09:39 +0100

    OOP: ES6 class, setters and getters, static methods, object.create

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -18,7 +18,7 @@ Creating prototypes
 4 OOP principals still valid (abstraction, encapsulation, inheritance, polymorphism)
 */
 
-// Constructor functions
+// ****************** Constructor functions
 // build an object by using function
 // just usual function the difference is that we call it with 'new'
 
@@ -53,7 +53,7 @@ const jay = 'Jay';
 console.log(marcin instanceof Person);
 console.log(jay instanceof Person);
 
-// Prototypes
+// ****************** Prototypes
 // Person.prototype - all objects that are created with PErson constructor function will have an access to prototype property
 // only one function exists but each instance has an access to it
 // we do not keep this method in each instance
@@ -108,3 +108,126 @@ console.log(h1.__proto__);
 
 // function has also prototype
 console.dir(x => x + 1);
+
+// ****************** ES6 classes
+// class expression
+// const PersonCl = class {}
+
+// class declaration
+// in general it will create the same as function constructor with prototype chain
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // It will be still in prototype not in an object
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2022 - this.birthYear;
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+
+  // static method
+  static hey() {
+    console.log('hey there!');
+  }
+}
+
+// const jessica = new PersonCl('Jessica', 1996);
+const jessicaFull = new PersonCl('Jessica Davis', 1996);
+// console.log(jessica);
+console.log(jessicaFull);
+console.log(jessicaFull.age);
+
+const walter = new PersonCl('Walter White', 1965);
+console.log(walter);
+
+// PersonCl.prototype.greet = function() {
+//   console.log(`Hey ${this.firstName}`);
+// }
+
+// 1. Classes are NOT hoisted (we cannot use them before declaration)
+// 2. Classes are first-class citizens (can be passed to functions and returned by functions)
+// 3. Classes are executed in strict mode
+
+// ****************** Setters and getters
+const accounts = {
+  owner: 'marcin',
+  movements: [200, 300, 530, 120],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(movement) {
+    this.movements.push(movement);
+  },
+};
+
+console.log(accounts.latest);
+accounts.latest = 876;
+console.log(accounts.latest);
+console.log(accounts);
+
+// ****************** Static methods
+/*
+Array.from - from method is attached to Array constructor and cannot be used on array
+[1,2,3].from() will not work
+arrays does nto inherit this method as it is not attached to a prototype
+
+the same with Number.parseFloat()
+*/
+Person.hey = function () {
+  console.log('hey there!');
+};
+Person.hey();
+// marcin.hey(); - will throw an error
+
+//static hey() {
+//  console.log('hey there!');
+//}
+// in ES6 class
+PersonCl.hey();
+
+// ****************** Object.create
+// no prototypes, constructor and new
+const PersonProto = {
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+console.log(steven);
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+
+sarah.calcAge();
