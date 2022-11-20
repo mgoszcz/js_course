61ed6d47181c9f1bdcaf71b0db3ed62f71b125b6
mgoszczynski <marcin.goszczynski88@gmail.com>
2022-11-20 15:45:06 +0100

    Mapty App: geolocation, displaying map

---
+++ 15-Mapty/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// Getting geolocation from browser API
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`Your position is ${latitude}, ${longitude}`);
+      console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      //For dislaying map we use Leaflet, 'L' is namespace of it (global variable)
+
+      // First setView argument (array) is position and second is zoom
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could nto get your position');
+    }
+  );
+}
