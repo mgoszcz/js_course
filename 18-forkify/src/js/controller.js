a17c6d315a57549ddbbc78d52a48be23a663688b
mgoszczynski <marcin.goszczynski88@gmail.com>
2022-12-23 15:41:27 +0100

    Forkify App: DOM updating algorithm

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -24,6 +24,9 @@ const controlRecipes = async function () {
     if (!id) return;
 
     recipeView.renderSipnner();
+    // Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     // 1. Loading data
     await model.loadRecipe(id);
 
@@ -65,7 +68,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // Update the recipe view
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
--- 18-forkify/src/js/views/View.js
+++ 18-forkify/src/js/views/View.js
@@ -12,6 +12,47 @@ export default class View {
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
+  update(data) {
+    this._data = data;
+    // cretae new markup to compare with existing
+    const newMarkup = this._generateMarkup();
+    // convert string markup to DOM object
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+
+    // get current elements to compare with new
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    // comparison
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      // console.log(curEl, newEl.isEqualNode(curEl));
+
+      // check if nodes are different and check if first child is not an amepty string
+      // without the second check we will also update parent elements
+      // we want to update only text so we need to update element that first child contains text vlaue
+      // always text elements' first child is text
+      // Update changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        // console.log('❤️', newEl.firstChild.nodeValue.trim());
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Update changed ATTRIBUTES
+      // Only if element is different then iterate through all attributes and replace value to the new one
+      // in this case only different attributes wiull be those woth data difference, as other items were modified earlier
+      if (!newEl.isEqualNode(curEl)) {
+        console.log(Array.from(newEl.attributes));
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+      }
+    });
+  }
+
   _clear() {
     this._parentElement.innerHTML = '';
   }
