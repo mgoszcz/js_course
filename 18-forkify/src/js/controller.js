b64440ab422998a9b58dec57e10de641f8207dec
mgoszczynski <marcin.goszczynski88@gmail.com>
2022-12-27 19:27:07 +0100

    Forkify App:  challenges and documentation

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,3 +1,16 @@
+/*
+Improvements ideas
+easy
+1. Display number of pages between pagination buttons
+2. Ability to sort search results be duration or number of ingredients (might be not possible as search data not contains all of that information)
+3. perform validation of ingredients in view (before submitting)
+4. improve ingredient input (separate fields and allow more than 6 ingredients)
+more complex
+5. shopping list feature (button on recipe to add ingredients to a list)
+6. weekly meal plannign feature, assign recipes for the next 7 days and show them on a weekly calendar
+7. get nutrition data on each ingredients from spoonacular API and calculate total calories of recipe
+*/
+
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
